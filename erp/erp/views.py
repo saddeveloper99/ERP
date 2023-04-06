44240845d6b2b1d2ff6a8fb34a39d96a9fdaeb63
Kim Gwang woon <misili304@naver.com>
2023-04-07 04:04:00 +0900

    created add product btn

--- erp/erp/views.py
+++ erp/erp/views.py
@@ -16,3 +16,20 @@ def product_list_view(request):
             return render(request, 'erp/product_list.html', {'product': all_product})
         else:
             return redirect('/sign-in')
+
+    elif request.method == 'POST':
+        name = request.POST.get('name', '')
+        code = request.POST.get('code', '')
+        description = request.POST.get('description', '')
+        price = request.POST.get('price', '')
+        sizes = request.POST.get('sizes', '')
+
+        my_product = Product.objects.create(
+            name=name,
+            code=code,
+            description=description,
+            price=price,
+            sizes=sizes 
+            )
+        my_product.save()
+        return redirect('/product_list')
